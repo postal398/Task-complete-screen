e68704e47ceb12d916636ea47e43e1365b9ec8bf
Vadim <maslovvadim398@gmail.com>
2023-03-23 09:20:20 +0300

    already complete task

---
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                        Column(horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                            ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_task_completed),
+                                contentDescription = "compelte img",
+                            )
+                            Text(
+                                "All tasks completed",
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.padding(0.dp, 24.dp, 0.dp, 8.dp)
+                               )
+                            Text(
+                                "Nice work!",
+                                fontSize = 16.sp
+                            )
+                        }
+
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TaskManagerTheme {
+        Greeting("Android")
+    }
+}
